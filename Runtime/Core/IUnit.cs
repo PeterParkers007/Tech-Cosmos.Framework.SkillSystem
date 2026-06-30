6a8887372d58a9494f214b9860ab8a75d0d3ef13
PeterParkers007 <3427463164@qq.com>
2026-07-01 04:11:41 +0800

    更新 IUnit.cs

--- Runtime/Core/IUnit.cs
+++ Runtime/Core/IUnit.cs
@@ -5,8 +5,6 @@ namespace TechCosmos.SkillSystem.Runtime
     /// </summary>
     public interface IUnit<T> where T : class, IUnit<T>
     {
-        /// <summary>返回该单位支持的事件名称列表。</summary>
-        string[] GetSupportedEvents();
         /// <summary>使用完整上下文触发指定事件。</summary>
         void TriggerEvent(string eventName, SkillContext<T> context);
         /// <summary>向单位注册技能。</summary>
